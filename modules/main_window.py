3c0590a73838315fbc6662a539c8f872bd92fdd6
Illya <121565465+IllyaEpik@users.noreply.github.com>
2024-11-14 22:20:27 +0200

    place ship

--- modules/main_window.py
+++ modules/main_window.py
@@ -3,7 +3,7 @@ pygame.init()
 import modules.buttons as m_buttons
 import modules.data as m_data
 import modules.images
-
+import modules.ships as m_ships
 class Screen():
     def __init__(self):
         self.clock = pygame.time.Clock()
@@ -18,13 +18,24 @@ class Screen():
                 if event.type == pygame.QUIT:
                     game = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    m_buttons.input.activate(event) 
-                    m_buttons.button_start.button_start(event)
+                    if m_data.progression != "menu":
+                        for ship in m_data.all_ships:
+                            ship.place(event)
+                    else:
+                        m_buttons.input.activate(event) 
+                        m_buttons.button_start.button_start(event)
+                    if m_data.progression == "pre-game":
+                        for ship in m_data.all_ships:
+                            ship.activate(event)
                 if event.type == pygame.KEYDOWN:
                     m_buttons.input.edit(event)
             self.screen.fill((255,255,255))
             for sprite in m_data.list_blits[m_data.progression]:
                 sprite.blit(self.screen)
+                
+            if m_data.progression != "menu":
+                for ship in m_data.all_ships:
+                    ship.blit(self.screen)
             pygame.display.flip()
             self.clock.tick(60)
 
