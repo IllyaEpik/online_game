a3e5e8a04b34675b24e82eeb0869f69d449d3b73
Illya <121565465+IllyaEpik@users.noreply.github.com>
2024-11-17 19:19:16 +0200

    place ship

--- modules/buttons.py
+++ modules/buttons.py
@@ -1,9 +1,10 @@
-import pygame
+import pygame , socket, random
 from modules.images import Image
 import modules.data as m_data
+from modules.ships import Ship 
 
 class Button(Image):
-    def __init__(self, fun = None, width = 100, height = 100, x= 0, y= 0, name = "", progression = "menu", text : str ="Button" ):
+    def __init__(self, fun = None, width = 100, height = 100, x= 0, y= 0, name = "", progression = "menu", text: str ="Button", size = 65):
         Image.__init__(self, width=width, height=height, x=x, y=y, name=name, progression=progression)
         self.WIDTH_BUT = width
         self.HEIGHT_BUT = height
@@ -13,17 +14,18 @@ class Button(Image):
         self.X_text = 497
         self.Y_text = 350
         self.COLOR = (0, 0, 0)
-        self.FONT = pygame.font.SysFont("OldEnglishText", 65)
-        self.TEXT = text 
+        self.FONT = pygame.font.SysFont("OldEnglishText", size)
+        self.TEXT = text  
         self.rect = pygame.Rect(x,y,width,height)
 
     def button_start(self, event):
         if self.rect.collidepoint(event.pos):
-            m_data.id = input.TEXT.split(" ")[1]
+            m_data.ip = input.TEXT.split(" ")[1]
             m_data.progression =  "pre-game"
-            print(m_data.id)
+            print(m_data.ip)
     def blit(self, screen):
-        Image.blit(screen=screen, self = self)
+        if self.name != "":
+            Image.blit(screen=screen, self = self)
         size = self.FONT.size(self.TEXT)
         y = self.y + self.height/2-size[1]/2
         x = self.x + self.width/2-size[0]/2
@@ -66,5 +68,82 @@ class Input(Image):
                 self.TEXT += key
 
         self.RENDER_TEXT = self.FONT.render(self.TEXT, True, self.COLOR)
+class Auto(Image):
+    def __init__(self, width: int, height: int, x: int, y: int, name='', progression: str = "pre-game"):
+        super().__init__(width, height, x, y, name, progression)
+    def randomship():
+        count_ships = 0 
+        while True:
+            row = random.randint(0, 9)
+            cell = random.randint(0, 9)
+            if m_data.my_field[row][cell] ==  0:
+                ship = Ship(x = 0, y = 0, cell = cell, row = row)
+                count_ships += 1
+                m_data.my_field[row][cell] = 1
+            if count_ships == 4:
+                break
+            
+        count_ships = 0
+        while True:
+            row = random.randint(0, 9)
+            cell = random.randint(0, 9)
+            try:
+                if m_data.my_field[row][cell] ==  0: 
+                    if m_data.my_field[row][cell+1] ==  0: 
+                        m_data.my_field[row][cell] = 2
+                        m_data.my_field[row][cell+1] = 2
+                        ship = Ship(x = 0, y = 0, cell = cell, row = row, name= "2")
+                        count_ships += 1
+            except:
+                pass
+            if count_ships == 3:
+                break
+
+        count_ships = 0
+        while True:
+            row = random.randint(0, 9)
+            cell = random.randint(0, 9)
+            try:
+                if m_data.my_field[row][cell] ==  0:
+                    if m_data.my_field[row][cell+1] ==  0:
+                        if m_data.my_field[row][cell+2] ==  0:
+                            m_data.my_field[row][cell+2] = 3
+                            m_data.my_field[row][cell+1] = 3
+                            m_data.my_field[row][cell] = 3
+                            ship = Ship(x = 0, y = 0, cell = cell, row = row, name= "3")
+                            count_ships += 1
+            except:
+                print("deniska sosiska")
+            if count_ships == 2:
+                break
+        
+        count_ships = 0
+        while True:
+            row = random.randint(0, 9)
+            cell = random.randint(0, 9)
+            try:
+                if m_data.my_field[row][cell] ==  0:
+                    if m_data.my_field[row][cell+1] ==  0:
+                        if m_data.my_field[row][cell+2] ==  0:
+                            if m_data.my_field[row][cell+3] ==  0:
+                                m_data.my_field[row][cell+3] = 4
+                                m_data.my_field[row][cell+2] = 4
+                                m_data.my_field[row][cell+1] = 4
+                                m_data.my_field[row][cell] = 4
+                                ship = Ship(x = 0, y = 0, cell = cell, row = row, name= "4")
+                                count_ships += 1
+            except:
+                pass
+            if count_ships == 1:
+                break
+
+
+
+
 input = Input(width = 551, height = 165, x = 366 , y = 531, name = "button_start")
 button_start = Button(width = 402 , height = 120, x = 435, y = 343, name = "button_start", text= "Start Game")
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+ip = Button(x = 981, y = 59, width = 281, height = 84, name = "button_start", text = ip, size = 50)
+text_ip = Button(x = 981, y = 10, width = 281, height = 45, text = "user ip", size = 50)
+m_data.list_blits["menu"].append(text_ip)
--- modules/images.py
+++ modules/images.py
@@ -2,7 +2,7 @@ import pygame, os
 import modules.data as m_data
 
 class Image():
-    def __init__(self, width: int, height: int, x: int, y: int, name: str, progression: str= "menu"):
+    def __init__(self, width: int, height: int, x: int, y: int, name = '', progression: str= "menu"):
         self.width = width
         self.height = height
         self.x = x
@@ -15,19 +15,24 @@ class Image():
     def update_image(self):
         try:
             self.image = pygame.image.load(os.path.abspath(f"{__file__}/../../images/{self.name}.png"))
+            print(self.name)
             self.image = pygame.transform.scale(self.image, (self.width, self.height))
+            print(self.name)
+
             if self in m_data.list_blits:
                 pass
             else:
                 m_data.list_blits[self.progression].append(self)
         except:
-            print("Error: image")
+            print("Error: image",self.name)
 
     def blit(self, screen):
+    
         screen.blit(self.image, (self.x, self.y))
+    
 
 background = Image(width = 1280, height = 851, x = 0, y = 0, name = "background")
 playing_field = Image(width = 1280, height = 832, x = 0, y = 0, name = "playing_field", progression = "pre-game")
 right_ship = Image(width = 388, height = 318, x = 903, y = 243, name = "right_ship")
 left_ship = Image(width = 381, height = 316, x = 10, y = 260, name = "left_ship")
-hat = Image(width = 187, height = 179, x= 370, y = 252, name = "hat")
+hat = Image(width = 187, height = 150, x= 370, y = 252, name = "hat")
